89f4da043d7af0ecdf75962dc04e1a47f8ad7c47
zachary kraimer <zachary kraimer>
2017-06-19 08:29:57 -0700

    Start of the project

---
+++ Scheduling/src/Order.java
@@ -0,0 +1,59 @@
+import java.util.Date;
+
+public class Order {
+
+	private String customer;
+	private String item;
+	private int quantity;
+	private double price;
+	private Date due;
+	
+	public Order(String string, String string2, int i, double d, Date endDay) {
+		customer = string;
+		item = string2;
+		quantity = i;
+		price = d;
+		due = endDay;
+	}
+	
+	public String getCustomer() {
+		return customer;
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public Object getPrice() {
+		return price;
+	}
+
+	public Date getDueDate() {
+		return due;
+	}
+
+	public void setCustomer(String aValue) {
+		customer = aValue;
+	}
+
+	public void setItem(String aValue) {
+		item = aValue;
+	}
+
+	public void setQuantity(Integer aValue) {
+		quantity = aValue;
+	}
+
+	public void setPrice(Double aValue) {
+		price = aValue;
+	}
+
+	public void setDueDate(Date aValue) {
+		due = aValue;
+	}
+	
+}
